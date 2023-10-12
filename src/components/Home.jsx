50608afea3e2b6025890785b96a2d8b4ea24a9ef
Azim Dadi <dadiazim11@gmail.com>
2023-10-12 19:13:58 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import Img from "../../public/img/1.jpg";
+import TodayWeather from "./TodayWeather";
+import TodayHighlights from "./TodayHighlights";
+import axios from "axios";
+
+const Home = () => {
+  const [location, setLocation] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  // console.log(location);
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        const response = await axios.get("https://ipapi.co/json/");
+        const data = response.data;
+        setLocation(data.city);
+      } catch (error) {
+        console.error("Error fetching location:", error);
+      }
+    };
+
+    fetchLocation();
+  }, []);
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&units=metric`
+      );
+      const data = response.data;
+      setWeatherData(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching Weather data:", error);
+      alert("Location Not Found");
+    }
+    setLocation("");
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <h1 className="text-blue-900 font-bold text-4xl pt-10 pb-7">
+        Weather App
+      </h1>
+      <div>
+        <form className="pb-8">
+          <input
+            onChange={(event) => setLocation(event.target.value)}
+            type="text"
+            value={location}
+            placeholder="Enter Location"
+            className="border rounded-l-md p-3 text-base border-gray-500 focus:outline-blue-500 capitalize"
+          />
+
+          <button
+            onClick={handleSearch}
+            className="border rounded-r-md sm:p-3 sm:w-36 p-3 w-24 border-blue-500 bg-blue-500 text-white font-medium text-base"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+
+      {/* //Weather and Highlights */}
+      {weatherData && (
+        <>
+          <span className="text-2xl font-semibold text-purple-800">
+            Location: {weatherData.name}
+          </span>
+          <div className="flex flex-col sm:flex-row    ">
+            <span className="m-8 pl-2 pt-4 sm:pl-10 sm:pt-10  rounded-lg border-purple-800 border ">
+              <TodayWeather weatherData={weatherData} />
+            </span>
+            <span className="m-8 pl-11 pt-4 sm:ml-10 sm:p-10  rounded-lg border border-purple-800">
+              <TodayHighlights weatherData={weatherData} />
+            </span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/TodayWeather.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const TodayWeather = ({ weatherData }) => {
+//   console.log(weatherData);
+
+  // if (!weatherData) {
+
+  // }
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold  text-purple-800 flex justify-center">
+        Today's Weather
+      </h2>
+      <div className="flex flex-row pt-5   ">
+        <div className="flex flex-col justify-center  ">
+          <img
+            src={`https://api.openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+            alt="img"
+            className="object-cover h-20 w-20 sm:h-24 sm:w-20 rounded-md mb-28"
+          />
+        </div>
+        <div className="flex flex-col p-10 ">
+          <p className="text-4xl font-semibold pb-2 -mt-4">
+             {weatherData.main.temp.toFixed(0)}&#8451;
+          </p>
+          <p className="text-lg ">
+            Feels Like: {weatherData.main.feels_like.toFixed(0)}&#8451;
+          </p>
+          <p className="text-lg pb-4 border-b-[1px] border-black capitalize">
+          Description: {weatherData.weather[0].description}
+          </p>
+          <p className="text-lg pt-4 pb-2">
+            Date:
+            {new Date().toLocaleDateString(undefined, {
+              weekday: "long",
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            })}
+          </p>
+          <p className="text-lg pt-2 pb-2">Time:{new Date().toLocaleTimeString()}</p>
+          <p className="text-lg pt-2 pb-8">Location: {weatherData.name}, {weatherData.sys.country}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodayWeather;
